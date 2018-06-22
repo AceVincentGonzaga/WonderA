0b19f2562489a40cead96107d15af0c3fedc1caf
Unknown <acegwapog@gmail.com>
2018-06-22 21:36:37 +0800

    Initial Commit

---
+++ why/app/src/main/java/com/example/user/wanderauser/Main2Activity.java
@@ -0,0 +1,24 @@
+package com.example.user.wanderauser;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class Main2Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+
+
+
+    }
+}
